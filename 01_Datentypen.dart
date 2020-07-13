8332463e0a273f45061ba11ead32f329b69908e8
JUD0ZWERG <68183445+JUD0ZWERG@users.noreply.github.com>
2020-07-13 14:49:43 +0200

    Create 01_Datentypen.dart

---
+++ 01_Datentypen.dart
@@ -0,0 +1,16 @@
+main() {
+  int number = 0;
+  int nuomber = 1;
+  
+  print(number);
+  print(nuomber);
+  
+  double duble = 4.2;
+  double duoble = 6.9;
+  
+  print(duble);
+  print(duoble);
+  
+  String string = "Hallo ich bin ein String";
+  print(string);
+}
