c4225dc8cc25f4ed8b185c6a3e1754fac486fb8f
JUD0ZWERG <68183445+JUD0ZWERG@users.noreply.github.com>
2020-07-14 10:33:00 +0200

    Create 02_Stringverarbeitung.dart

---
+++ 02_Stringverarbeitung.dart
@@ -0,0 +1,11 @@
+main() {
+  String meinName = "Anton";
+  print("Mein Name ist $meinName");
+  
+  double dub1 = 4.2;
+  String strdub1 = dub1.toString();
+  print(strdub1);
+  int laength = meinName.length;
+  print("Mein Name ist $laength Buchstaben lang");
+    
+}
